459c0b68e21d5fcc77da73a957dc06330af8fe9f
Colin Moore <cmoore1711@gmail.com>
2018-03-20 14:31:16 -0400

    Update piUpdate.sh

--- MASTER/Resources/piUpdate.sh
+++ MASTER/Resources/piUpdate.sh
@@ -2,8 +2,7 @@
 
 cd Desktop/FTY_MASTER1/MASTER
 sudo git pull
-sudo apt-get update -y
-sudo apt-get upgrade -y
+
 sudo apt-get install rdate
 sudo apt-get -y remove xscreensaver
 sudo /etc/init.d/ntp stop
