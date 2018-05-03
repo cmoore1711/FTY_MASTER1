e8a8c1c3f102fd2b39645e2b1b45a13d47041c5f
Colin Moore <cmoore1711@gmail.com>
2018-05-03 15:57:02 -0400

    Add new python features!

---
+++ MASTER/Resources/pythonTimeSync.py
@@ -0,0 +1,24 @@
+import os
+import time
+import mysql.connector
+
+
+
+
+while(True):
+
+    try:
+        cnx = mysql.connector.connect(user='cronos', password='zeusdad',host='10.10.9.107')
+        cursor = cnx.cursor()
+        time.sleep(60)
+
+        selectTimeString = "SELECT now();"
+        
+        
+        cursor.execute(selectTimeString)
+        currTime = str(cursor.fetchone()[0])
+     
+        changeTimeString = "sudo date -s " + '"' + currTime +'"'
+        os.system(changeTimeString)
+        cnx.close()
+    
---
+++ MASTER/Resources/updateFTY.py
@@ -0,0 +1,6 @@
+import os
+
+
+try:
+    updateString = "sudo sh Desktop/FTY_MASTER1/MASTER/Resources/piUpdate.sh"
+    os.system(updateString)
