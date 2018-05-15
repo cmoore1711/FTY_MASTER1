69c076a7c1caf8291ddadb4c616ddb15dd5df244
Colin Moore <cmoore1711@gmail.com>
2018-05-15 10:35:16 -0400

    Update updateOS.sh

--- MASTER/Resources/updateOS.sh
+++ MASTER/Resources/updateOS.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install rdate
-sudo apt-get -y remove xscreensaver
-sudo /etc/init.d/ntp stop
-sudo /etc/init.d/ntp start
+sudo apt-get -y update
+sudo apt-get -y upgrade 
